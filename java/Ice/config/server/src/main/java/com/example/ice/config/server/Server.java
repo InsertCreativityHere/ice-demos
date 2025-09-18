d7e613f152878bed7f8187092b4df7a13a3fbda2
Austin Henriksen <austin@zeroc.com>
2025-09-18 14:25:46 -0400

    Small cleanup to Java CtrlC hooks.

--- java/Ice/config/server/src/main/java/com/example/ice/config/server/Server.java
+++ java/Ice/config/server/src/main/java/com/example/ice/config/server/Server.java
@@ -21,10 +21,6 @@ class Server {
         // set in args override these properties.
         try (Communicator communicator = Util.initialize(args, initData)) {
 
-            // Register a shutdown hook that calls communicator.shutdown() when the user shuts down the server with
-            // Ctrl+C or similar. The shutdown hook thread also waits until the main thread completes its cleanup.
-            shutdownCommunicatorOnCtrlC(communicator, Thread.currentThread());
-
             // Create an object adapter that listens for incoming requests and dispatches them to servants.
             ObjectAdapter adapter = communicator.createObjectAdapter("GreeterAdapter");
 
@@ -34,6 +30,10 @@ class Server {
             // Start dispatching requests.
             adapter.activate();
             System.out.println("Listening on port 4061...");
+
+            // Register a shutdown hook that calls `communicator.shutdown()` when the user presses 'Ctrl+C' or similar.
+            // The shutdown hook thread waits until the main thread completes its cleanup.
+            shutdownCommunicatorOnCtrlC(communicator, Thread.currentThread());
 
             // Wait until the communicator is shut down. Here, this occurs when the user presses Ctrl+C.
             communicator.waitForShutdown();
@@ -48,10 +48,7 @@ class Server {
             // Wait until the main thread completes.
             try {
                 mainThread.join();
-            } catch (InterruptedException e) {
-                // No code interrupts the shutdown hook thread in this program.
-                assert false;
-            }
+            } catch (InterruptedException ignored) {}
         }));
     }
 }
